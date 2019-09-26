e6defdf4bdd31e5001287a14165db1e18b77edce
mekamdan <m.alencar.v@outlook.com>
2019-09-26 00:52:25 -0300

    env

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/d/Users/mekam/Documentos/Documents/Eu/Hackaton 2019.2/AlunoWeb/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
